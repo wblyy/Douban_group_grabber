7ef28dd9406e4eba55767e773cac0643f1ddb3d3
Balloon_Wang <wblyy0911@gmail.com>
2014-11-20 18:36:08 +0800

    proxy_handler = urllib2.ProxyHandler({'http':random.choice(proxy_dict)})

--- douban_group_grabber.py
+++ douban_group_grabber.py
@@ -33,7 +33,8 @@ proxy_dict=['http://113.11.198.163:2223/',
 #113.11.198.[163-169] 2223
 proxy_auth_handler = urllib2.HTTPBasicAuthHandler()
 proxy_auth_handler.add_password('realm', 'host', 'root', 'kingate')
-proxy_handler = urllib2.ProxyHandler({'http': 'http://113.11.198.163:2223/'})
+#proxy_handler = urllib2.ProxyHandler({'http': 'http://113.11.198.163:2223/'})
+proxy_handler = urllib2.ProxyHandler({'http':random.choice(proxy_dict)})
 #opener = urllib2.build_opener(proxy_handler, proxy_auth_handler)
 # This time, rather than install the OpenerDirector, we use it directly:
 #f = urllib2.build_opener(proxy_handler, proxy_auth_handler).open('http://www.douban.com').read()
