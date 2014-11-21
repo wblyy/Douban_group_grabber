9ef974b746f7977bcae32e6e0ea093ed4357f870
Balloon_Wang <wblyy0911@gmail.com>
2014-11-21 10:46:54 +0800

    proxy自己也有问题

--- proxy_test.py
+++ proxy_test.py
@@ -1,9 +1,10 @@
 #-*-coding:utf-8-*-
 import requests
+import random
 proxy_dict=['http://113.11.198.163:2223/',
-			'http://113.11.198.164:2223/',
-			'http://113.11.198.165:2223/',
-			'http://113.11.198.166:2223/',
+			#'http://113.11.198.164:2223/',#
+			#'http://113.11.198.165:2223/',#
+			#'http://113.11.198.166:2223/',#
 			'http://113.11.198.167:2223/',
 			'http://113.11.198.168:2223/',
 			'http://113.11.198.169:2223/',
@@ -11,5 +12,5 @@ proxy_dict=['http://113.11.198.163:2223/',
 
 
 r = requests.get("http://www.douban.com", 
-                 proxies={"http": "http://113.11.198.164:2223"})
+                 proxies={"http": random.choice(proxy_dict)})
 print(r.text)
